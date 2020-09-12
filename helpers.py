d88cc5c3098639745dadd9b86d9d4571bcfabbef
Karthick Srinivasan <S_kathick@cat.com>
2020-09-12 11:11:38 +0530

    .

--- helpers.py
+++
@@ -1,7 +0,0 @@
-from colorama import init,Fore,Back,Style
-
-def display(message,is_Warning=False):
-    if is_Warning:
-        print(Fore.RED+message)
-    print(Fore.BLUE+message)
-    
---
+++ in28_1to100.py
@@ -0,0 +1,36 @@
+
+# def multiplication():
+#     number = input('Enter the number for which multiplication tables has to be displayed: ')
+#     for i in range(1,11):
+    # print(number + ' * ' +str(i) + ' = ' + str((int(number)*i)))
+    #print("{0}*{1}={2}".format(int(number),i,int(number)*i))
+        # print(f"{int(number)}*{i}={int(number)*i}")
+
+# multiplication()
+
+# def printMultiplicationUpto():
+#     n=int(input('enter the number'))
+#     upto=int(input('enter upto'))
+#     for i in range(1,upto):
+#         print(f"{n}*{upto}={n*upto}")
+
+# printMultiplicationUpto()
+
+# def sum_of_three_integers(*values):
+#     sum=0
+#     for i in values:
+#         sum+=i
+#     return sum
+
+# total=sum_of_three_integers(2,3,5)
+# print(total)
+
+# def third_angle():
+#     angle3=0
+#     angle1=int(input('First angle: '))
+#     angle2=int(input('Second angle: '))
+#     angle3=180-angle1-angle2
+#     return angle3
+
+# answer=third_angle()
+# print(answer)
